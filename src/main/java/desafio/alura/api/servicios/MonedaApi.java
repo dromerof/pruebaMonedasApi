391261391d50e1a2c8f3694cdc7df7b83a362453
Daniel Romero <dandres508@gmail.com>
2024-10-01 15:09:03 -0500

    empleando el .env

--- src/main/java/desafio/alura/api/servicios/MonedaApi.java
+++ src/main/java/desafio/alura/api/servicios/MonedaApi.java
@@ -1,4 +1,15 @@
 package desafio.alura.api.servicios;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
+
 public class MonedaApi {
+    private final String API_URL;
+    private final String API_KEY;
+
+    public MonedaApi(){
+        Dotenv dotenv = Dotenv.load();
+        this.API_KEY = dotenv.get("API_KEY");
+        this.API_URL = dotenv.get("API_URL");
+    }
 }
