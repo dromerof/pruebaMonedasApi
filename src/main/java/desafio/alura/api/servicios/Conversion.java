220d1499a71155f34854bcc08aa8d0dcfcddc9d8
Daniel Romero <dandres508@gmail.com>
2024-10-01 14:56:04 -0500

    Constructor

--- src/main/java/desafio/alura/api/servicios/Conversion.java
+++ src/main/java/desafio/alura/api/servicios/Conversion.java
@@ -1,4 +1,13 @@
 package desafio.alura.api.servicios;
 
+import desafio.alura.api.modelos.Moneda;
+
+import java.time.LocalDateTime;
+
 public class Conversion {
+    private Moneda monedaInicial;
+    private Moneda monedaFinal;
+    private double cantidad;
+    private double resultado;
+    private LocalDateTime fechaConversion;
 }
